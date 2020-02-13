a547f5829ce071492fbcce66136396cabd8d1b84
Marko <stankovicmarko333@gmail.com>
2020-02-13 21:25:25 +0100

    TASK5.1

---
+++ Checkin-script.js
@@ -0,0 +1,27 @@
+
+ var pom = 3; 
+//  pom iz valiable for insert id
+
+function CheckChar(){
+
+   
+    if(pom === 4)
+    //  call function for check id in base 
+    document.getElementById("d").innerHTML = "Uspesno prosao test broja karaktera";
+
+    else{
+        if(pom < 4)
+        alert("Uneli ste nedovoljan broj karaktera");
+        else
+        alert("Uneli ste previse karaktera, pokusajte ponovo");
+        
+        //  Insert functio for delate caracter or only delete caracter in string
+        
+    }
+    
+    
+
+
+  
+
+}
