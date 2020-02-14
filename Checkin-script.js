0c9df468be20612c7c33f269c8ade7f3ab1eec4b
Marko <stankovicmarko333@gmail.com>
2020-02-14 15:15:28 +0100

    drugiTrask

--- Checkin-script.js
+++ Checkin-script.js
@@ -1,12 +1,13 @@
 
- 
+var pom; 
 function CheckChar(){
-    var pom = document.getElementById("input").value;     //  pom iz valiable for insert id
-    if(pom.length === 4){
-    //  call function for check id in base
+    pom = document.getElementById("input").value;   //  pom iz valiable for insert id
+    if(pom.length == 4){
+                 
+                CheckPIN();    //  call function for check id in base
      
-    // /document.getElementById("d").innerHTML = "Uspesno prosao test broja karaktera";  onlu for test
-    alert("Uneli ste dovoljan broj karaktera");
+                                // /document.getElementById("d").innerHTML = "Uspesno prosao test broja karaktera";  onlu for test
+                
     }
     else{
         if(pom.length < 4)
@@ -16,4 +17,29 @@ function CheckChar(){
         pom = ""; //  Insert functio for delate caracter or only delete caracter in string pom = ""   
     }
     document.getElementById("input").value = "";
+}
+
+
+
+function CheckPIN(){
+
+    var niz = new Array(); // there iz line for insert conection vith sql base 
+    niz = [1234,2345,3456,4567]; //test
+    var p = false;
+
+    for (let i = 0; i < niz.length; i++) {
+        if(niz[i] == pom)
+        {
+            document.getElementById("d").innerHTML = "Uspesno ste se prijavili kao " + pom;
+            p = true;
+
+        
+        }
+        else if(p){
+            alert("Uneli ste pogresan pin broj karaktera");
+        }
+        
+        
+    }
+
 }
