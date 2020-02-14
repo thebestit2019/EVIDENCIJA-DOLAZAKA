866944e4eb3c5292653963cab2b0798f6114129d
nikola631 <nikolastankovic631@gmail.com>
2020-02-14 16:06:07 +0100

    Odradjeni taskovi 4, 4.1 i 6

--- Checkin-script.js
+++ Checkin-script.js
@@ -19,11 +19,67 @@ function startTime() {
     sec = checkTime(sec);
     document.getElementById('time').value = hour + ":" + min + ":" + sec;
     let t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+}
+function checkTime(i) {
+if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
-  }
+}
 
+let display = document.getElementById("display");
+let PIN = '';
 
+document.getElementById("btn1").addEventListener("click", function () {
+    input(document.getElementById("btn1").value)
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+    input(document.getElementById("btn2").value)
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+    input(document.getElementById("btn3").value)
+});
+
+document.getElementById("btn4").addEventListener("click", function () {
+    input(document.getElementById("btn4").value)
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+    input(document.getElementById("btn5").value)
+});
+
+document.getElementById("btn6").addEventListener("click", function () {
+    input(document.getElementById("btn6").value)
+});
+
+document.getElementById("btn7").addEventListener("click", function () {
+    input(document.getElementById("btn7").value)
+});
+
+document.getElementById("btn8").addEventListener("click", function () {
+    input(document.getElementById("btn8").value)
+});
+
+document.getElementById("btn9").addEventListener("click", function () {
+    input(document.getElementById("btn9").value)
+});
+
+document.getElementById("btn0").addEventListener("click", function () {
+    input(document.getElementById("btn0").value)
+});
+
+function input(value){
+    display.value += value;
+    PIN += value;
+    console.log("PIN " + PIN);
+    
+}
+
+document.getElementById("reset").addEventListener("click", reset);
+
+function reset(){
+    display.value = '';
+    PIN = '';
+    console.log("PIN " + PIN);
+}
   
